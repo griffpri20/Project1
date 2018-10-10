41f6fa5fc7ecd73c1077919e20e9a63fbb9e109a
Griffie Prime <gprime@umich.edu>
2018-10-10 16:09:52 -0400

    Got working

--- project1-206.py
+++ project1-206.py
@@ -5,55 +5,109 @@ from datetime import date
 
 
 def getData(file):
-# get a list of dictionary objects from the file
-#Input: file name
-#Ouput: return a list of dictionary objects where
-#the keys are from the first row in the data. and the values are each of the other rows
-
-	pass
+    # get a list of dictionary objects from the file
+    #Input: file name
+    #Ouput: return a list of dictionary objects where
+    #the keys are from the first row in the data. and the values are each of the other rows
+    inFile = open(file, 'r')
+    list = []
+    line1 = inFile.readline()
+    splitLine1 = line1.split(',')
+    
+    first = splitLine1[0]
+    last = splitLine1[1]
+    email = splitLine1[2]
+    course = splitLine1[3]
+    DOB = splitLine1[4].strip('\n')
+    
+    lines = inFile.readlines()
+    inFile.close()
+    
+    for y in lines:
+        x = y.split(',')
+        dic = {
+            first: x[0],
+            last: x[1],
+            email: x[2],
+            course: x[3],
+            DOB: x[4]
+        }
+        list.append(dic)
+    return list
 
 def mySort(data,col):
-# Sort based on key/column
-#Input: list of dictionaries and col (key) to sort on
-#Output: Return the first item in the sorted list as a string of just: firstName lastName
+    # Sort based on key/column
+    #Input: list of dictionaries and col (key) to sort on
+    #Output: Return the first item in the sorted list as a string of just: firstName lastName
+    x = sorted(data, key = lambda y: y[col])
+    return '{} {}'.format(x[0]['First'], x[0]['Last'])
 
-	pass
+
 
 
 def classSizes(data):
-# Create a histogram
-# Input: list of dictionaries
-# Output: Return a list of tuples sorted by the number of students in that class in
-# descending order
-# [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
-
-	pass
+    # Create a histogram
+    # Input: list of dictionaries
+    # Output: Return a list of tuples sorted by the number of students in that class in
+    # descending order
+    # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
+    d = {}
+    for x in data:
+        year = x['Class']
+        if year in d:
+            d[year] += 1
+        else:
+            d[year] = 1
+    return sorted(d.items(), key=lambda y:y[1], reverse=True)
 
 
 def findMonth(a):
-# Find the most common birth month form this data
-# Input: list of dictionaries
-# Output: Return the month (1-12) that had the most births in the data
-
-	pass
+    # Find the most common birth month form this data
+    # Input: list of dictionaries
+    # Output: Return the month (1-12) that had the most births in the data
+    d = {}
+    for x in a:
+        date = x['DOB']
+        q = date.split('/')
+        mth = q[0]
+        if mth in d:
+            d[mth] += 1
+        else:
+            d[mth] = 1
+    temp = sorted(d, key=lambda y: d[y], reverse = True)
+    return int(temp[0])
 
 def mySortPrint(a,col,fileName):
-#Similar to mySort, but instead of returning single
-#Student, the sorted data is saved to a csv file.
-# as fist,last,email
-#Input: list of dictionaries, col (key) to sort by and output file name
-#Output: No return value, but the file is written
-
-	pass
+    #Similar to mySort, but instead of returning single
+    #Student, the sorted data is saved to a csv file.
+    # as fist,last,email
+    #Input: list of dictionaries, col (key) to sort by and output file name
+    #Output: No return value, but the file is written
+    out = open(fileName, 'w')
+    x = sorted(a, key = lambda y: y[col])
+    for y in x:
+        out.write('{},{},{}\n'.format(y['First'], y['Last'], y['Email']))
+    out.close()
+    return
 
 def findAge(a):
-# def findAge(a):
-# Input: list of dictionaries
-# Output: Return the average age of the students and round that age to the nearest
-# integer.  You will need to work with the DOB and the current date to find the current
-# age in years.
+    # def findAge(a):
+    # Input: list of dictionaries
+    # Output: Return the average age of the students and round that age to the nearest
+    # integer.  You will need to work with the DOB and the current date to find the current
+    # age in years.
+    years = []
+    age = []
+    for x in a:
+        years.append(x['DOB'].split('/')[2].strip('\n'))
+    for x in years:
+        age.append(2018 - int(x))
+    answer = int(sum(age)) / int(len(age))
+    return round(answer)
 
-	pass
+
+
+
 
 
 ################################################################
@@ -62,56 +116,57 @@ def findAge(a):
 
 ## We have provided simple test() function used in main() to print what each function returns vs. what it's supposed to return.
 def test(got, expected, pts):
-  score = 0;
-  if got == expected:
-    score = pts
-    print(" OK ", end=" ")
-  else:
+    score = 0;
+        if got == expected:
+            score = pts
+print(" OK ", end=" ")
+    else:
     print (" XX ", end=" ")
-  print("Got: ",got, "Expected: ",expected)
-  return score
+        print("Got: ",got, "Expected: ",expected)
+        return score
 
 
 # Provided main() calls the above functions with interesting inputs, using test() to check if each result is correct or not.
 def main():
-	total = 0
-	print("Read in Test data and store as a list of dictionaries")
-	data = getData('P1DataA.csv')
-	data2 = getData('P1DataB.csv')
-	total += test(type(data),type([]),50)
+    total = 0
+    print("Read in Test data and store as a list of dictionaries")
+    data = getData('P1DataA.csv')
+    data2 = getData('P1DataB.csv')
+    total += test(type(data),type([]),50)
+    
+    print()
+    print("First student sorted by First name:")
+    total += test(mySort(data,'First'),'Abbot Le',25)
+    total += test(mySort(data2,'First'),'Adam Rocha',25)
+    
+    print("First student sorted by Last name:")
+    total += test(mySort(data,'Last'),'Elijah Adams',25)
+    total += test(mySort(data2,'Last'),'Elijah Adams',25)
+    
+    print("First student sorted by Email:")
+    total += test(mySort(data,'Email'),'Hope Craft',25)
+    total += test(mySort(data2,'Email'),'Orli Humphrey',25)
+    
+    print("\nEach grade ordered by size:")
+    total += test(classSizes(data),[('Junior', 28), ('Senior', 27), ('Freshman', 23), ('Sophomore', 22)],25)
+    total += test(classSizes(data2),[('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)],25)
+    
+    print("\nThe most common month of the year to be born is:")
+    total += test(findMonth(data),3,15)
+    total += test(findMonth(data2),3,15)
+    
+    print("\nSuccessful sort and print to file:")
+    mySortPrint(data,'Last','results.csv')
+    if os.path.exists('results.csv'):
+        total += test(filecmp.cmp('outfile.csv', 'results.csv'),True,20)
 
-	print()
-	print("First student sorted by First name:")
-	total += test(mySort(data,'First'),'Abbot Le',25)
-	total += test(mySort(data2,'First'),'Adam Rocha',25)
+    print("\nTest of extra credit: Calcuate average age")
+    total += test(findAge(data), 40, 5)
+    total += test(findAge(data2), 42, 5)
 
-	print("First student sorted by Last name:")
-	total += test(mySort(data,'Last'),'Elijah Adams',25)
-	total += test(mySort(data2,'Last'),'Elijah Adams',25)
-
-	print("First student sorted by Email:")
-	total += test(mySort(data,'Email'),'Hope Craft',25)
-	total += test(mySort(data2,'Email'),'Orli Humphrey',25)
-
-	print("\nEach grade ordered by size:")
-	total += test(classSizes(data),[('Junior', 28), ('Senior', 27), ('Freshman', 23), ('Sophomore', 22)],25)
-	total += test(classSizes(data2),[('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)],25)
-
-	print("\nThe most common month of the year to be born is:")
-	total += test(findMonth(data),3,15)
-	total += test(findMonth(data2),3,15)
-
-	print("\nSuccessful sort and print to file:")
-	mySortPrint(data,'Last','results.csv')
-	if os.path.exists('results.csv'):
-		total += test(filecmp.cmp('outfile.csv', 'results.csv'),True,20)
-
-	print("\nTest of extra credit: Calcuate average age")
-	total += test(findAge(data), 40, 5)
-	total += test(findAge(data2), 42, 5)
-
-	print("Your final score is " + str(total))
+print("Your final score is " + str(total))
 
 # Standard boilerplate to call the main() function that tests all your code
 if __name__ == '__main__':
     main()
+
